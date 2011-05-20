262bff427ed089d8ab063f7e52391874b3c24ecd
GorillaTron <jornandretangen@gmail.com>
2011-05-20 21:03:20 +0200

    structuring

--- models/soundcloudSettings.js
+++
@@ -1,9 +0,0 @@
-var SoundcloudSettings = {
-	client_id : "V0LK9YnuiV6o7mFCqqjDQw",
-	client_secret : "NrAiHyDKRjjFqBBylNXSGmJljJ0FLqY4VTiyCRcU",
-	redirect_uri : "http://localhost:3000/authorizeapp",
-	exchange_token_url : "https://api.soundcloud.com/oauth2/token",
-	api_url : "https://api.soundcloud.com"
-}
-
-exports.SoundcloudSettings = SoundcloudSettings;
